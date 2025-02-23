fa0949c8dd8692e5d34612161f7b6059505eca17
Mohankumar9944 <mrmohancmk2004@gmail.com>
2025-02-23 16:27:01 +0530

    Added gallery

--- js/about.js
+++ js/about.js
@@ -1,13 +1,13 @@
 
 
-// Reveal animations on scroll
+//Reveal animations on scroll
 function reveal() {
     const reveals = document.querySelectorAll('.reveal');
 
     reveals.forEach(element => {
         const windowHeight = window.innerHeight;
         const elementTop = element.getBoundingClientRect().top;
-        const elementVisible = 150;
+         const elementVisible = 150;
 
         if (elementTop < windowHeight - elementVisible) {
             element.classList.add('active');
