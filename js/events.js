0225f36db3a54c7c46962549e15a42c98b738b54
Mohankumar9944 <mrmohancmk2004@gmail.com>
2025-02-27 13:47:35 +0530

    added flipcard

--- js/events.js
+++ js/events.js
@@ -37,3 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+function flipCard(card) {
+    card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+}
