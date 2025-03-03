ce91b26ed22ce6456a85f216d8771dfa84bd31f6
Mohankumar9944 <mrmohancmk2004@gmail.com>
2025-03-03 22:08:30 +0530

    edited events section

--- js/events.js
+++ js/events.js
@@ -41,7 +41,7 @@ cards = document.querySelectorAll('.card')
 
 cards.forEach(card => {
     card.addEventListener('click', ()=>{
-        if(window.innerWidth<=600){
+        if(window.innerWidth<=545){
 
             card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
         }
